4f318a2e207b70518024cf09c0dbafbc8c51fc56
Matteo Chiara <36039934+matteo14c@users.noreply.github.com>
2025-06-13 15:38:16 +0200

    Add files via upload

---
+++ aScanMeth.sh
@@ -0,0 +1,24 @@
+# Path to your modkit executable
+MODKIT="../dist_modkit_v0.5.0_5120ef7/modkit"
+
+# Reference genome
+REF_GENOME="hs1.fa"
+
+
+# Input BAM file
+INPUT_BAM="phased_SAMPLE_P1.bam"
+PREFIX="res_"
+
+# chromosomes
+my_chr=("chr4" "chr10")
+
+for chr in "${my_chr[@]}"; do
+	OUT="${PREFIX}${chr}"
+	echo Processing $chr
+	$MODKIT pileup --region $chr --partition-tag HP --cpg --ref $REF_GENOME --combine-strands --filter-threshold C:0.75 --mod-threshold h:0.8 $INPUT_BAM $OUT
+	cp aScanMethMockUp.R $OUT
+        cd $OUT
+	Rscript aScanMethMockUp.R
+	rm aScanMethMockUp.R
+	cd ..	
+done
