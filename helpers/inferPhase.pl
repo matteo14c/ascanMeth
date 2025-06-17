e8a20d1e36deb0788f7fc29dcb5ba9a06d0eb2c9
Matteo Chiara <36039934+matteo14c@users.noreply.github.com>
2025-06-17 11:32:16 +0200

    Add files via upload

--- helpers/inferPhase.pl
+++ helpers/inferPhase.pl
@@ -23,9 +23,19 @@ while(<IN>)
 	$P1an=join(":",@P1an);
 	$P2an=join(":",@P2an);	
 	
-	$outH1=0;
-	$outH2=0;
+	# for child phasing
+	$cH1=0;
+	$cH2=0;
 	$sep="|";
+
+	# for dad's phasing
+	$pH1=$p11;
+	$pH2=$p12;
+	$sep="|";
+
+	# for mom's phasing
+	$mH1=$p21;
+	$mH2=$p22;
 
 	$c1P1= $c1 == $p11 || $c1 == $p12 ? 1 : 0;
 	$c2P1= $c2 == $p11 || $c2 == $p12 ? 1 : 0;
@@ -35,28 +45,45 @@ while(<IN>)
 
 	if ($c1P1==1 && $c1P2==0)
 	{
-		$outH1=$c1;
-		$outH2=$c2;
+		$cH1=$c1;
+		$cH2=$c2;
+		$pH1=$p11;
+		$pH2=$p12;
+
 		$phased=1
 	}elsif ($c1P1==0 && $c1P2==1){
-		$outH2=$c1;
-		$outH1=$c2;
+		$cH2=$c1;
+		$cH1=$c2;
+		$mH1=$p22;
+		$mH2=$p21;
+
 		$phased=1;
 	}elsif ($c2P1==1 && $c2P2==0){
-		$outH1=$c2;
-                $outH2=$c1;
-                $phased=1
+		$cH1=$c2;
+                $cH2=$c1;
+                $phased=1;
+		$pH1=$p12;
+                $pH2=$p11;
+
+
 	}elsif($c2P1==0 && $c2P2==1){
-		$outH2=$c2;
-                $outH1=$c1;
-                $phased=1
+		$cH2=$c2;
+                $cH1=$c1;
+                $phased=1;
+		$mH1=$p21;
+                $mH2=$p22;
+
 	}else{
-		$outH1=$c1;
-		$outH2=$c2;
+		$cH1=$c1;
+		$cH2=$c2;
 		$sep="/";
 	}
-	$out="$outH1$sep$outH2";
+	$C="$cH1$sep$cH2";
+	$P="$pH1$sep$pH2";
+	$M="$mH1$seo$mH2";
+	
 	print OUT "$chr\t$start\t$end\t$C\t$P1\t$P2\t$out:inv\n" if $out ne $C;
 	print OUT "$chr\t$start\t$end\t$C\t$P1\t$P2\t$out:cor\n" if $out eq $C;
-	print "$chr\t$start\t$p\t$ref\t$alt\t$score\t$filter\t$f\t$annots_F\t$out:$Can\t$P1:$P1an\t$P2:$P2an\n";
+	#$P1an, $P2an,$Can
+	print "$chr\t$start\t$p\t$ref\t$alt\t$score\t$filter\t$f\t$annots_F\t$C\t$P1\t$P2\n";
 }
