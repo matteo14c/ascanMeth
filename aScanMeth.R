7a234fb78c283ad4ca3855c93ae465601ee563e5
Matteo Chiara <36039934+matteo14c@users.noreply.github.com>
2025-06-19 17:49:17 +0200

    Add files via upload

--- aScanMeth.R
+++ aScanMeth.R
@@ -1,3 +1,4 @@
+
 # parameters
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -7,6 +8,8 @@ h2_val <- NULL
 n_val <- NULL
 c_val <- NULL
 
+
+# parse parameters
 if (length(args) > 0) {
 	i <- 1
 	while (i <= length(args)) {
@@ -29,9 +32,10 @@ if (length(args) > 0) {
 	}
 }
 
+# Add headers to bedmehtyl
 header<-c("chr","start","end","mod","score","strand","start","end","color","coverage","ratio","nmod","canonical","nother","ndelete","nfail","ndiff","nNo")
 
-# Input files. Could be parameters
+# Input files. 
 f1<-h1_val
 f2<-h2_val
 
@@ -40,8 +44,6 @@ covThr<-ifelse(is.null(c_val),5,c_val)
 numThr<-ifelse(is.null(n_val),20,n_val)
 # 
 
-# Add header to bedMethyl. Just to make it more readable and say what's what
-header<-c("chr","start","end","mod","score","strand","start","end","color","coverage","ratio","nmod","canonical","nother","ndelete","nfail","ndiff","nNo");
 
 # Read input files and attach header;
 meth1<-read.table(f1,header=FALSE);
@@ -49,7 +51,6 @@ meth2<-read.table(f2,header=FALSE);
 
 colnames(meth1)=header;
 colnames(meth2)=header;
-
 
 # Compute  common CpGs. Since the file are sorted and in the same order, they can be juxtaposed later
 commonPositionsM1<- meth1$chr %in% meth2$chr & meth1$start %in% meth2$start & meth1$end %in% meth2$end
@@ -65,17 +66,22 @@ commonPositionsM2<- meth2$chr %in% meth1$chr & meth2$start %in% meth1$start & me
 commonCpG<-data.frame(chr=meth1$chr[commonPositionsM1], start=meth1$start[commonPositionsM1], end=meth1$end[commonPositionsM1], mod=meth1$mod[commonPositionsM1], covM1=meth1$coverage[commonPositionsM1], nmodM1=meth1$nmod[commonPositionsM1], covM2=meth2$coverage[commonPositionsM2], nmodM2=meth2$nmod[commonPositionsM2])
 
 
-# Make a copy of the dataset
-# Being lazy, since filters applied later on
-#commonCpGunf<-commonCpG;
-
 # Filter coverage
 commonCpG<-commonCpG[commonCpG$covM1 >=covThr & commonCpG$covM2 >=covThr ,]
 
 # Compute a correction factor for uneven coverage! Why is coverage uneven anyway???
-corM1<-commonCpG$covM2/commonCpG$covM1
-commonCpG$covM1<-commonCpG$covM1*corM1
-commonCpG$nmodM1<-as.integer(commonCpG$nmodM1*corM1)
+#if (commonCpG$covM1 < commonCpG$covM2)
+#{
+	corM1<-commonCpG$covM2/commonCpG$covM1
+	commonCpG$covM1<-commonCpG$covM1*corM1
+	commonCpG$nmodM1<-as.integer(commonCpG$nmodM1*corM1)
+#}else if(commonCpG$coVM2 < commonCpG$covM1 ){
+#	corM2<-commonCpG$covM1/commonCpG$covM2
+#        commonCpG$covM2<-commonCpG$covM2*corM2
+#        commonCpG$nmodM2<-as.integer(commonCpG$nmodM2*corM2)
+#}
+
+
 
 # Compute equal methyl ratio (expected, this is m in the formula used by aScan)
 commonCpG$equalRatioMod=(commonCpG$nmodM1+commonCpG$nmodM2)/2
@@ -88,6 +94,7 @@ commonCpG$ascanStat= 2*(commonCpG$ascan1 + commonCpG$ascan2)
 # log meth ratio: used for plots
 commonCpG$logMethRatio= log2(commonCpG$nmodM1/commonCpG$nmodM2)
 
+write.table(commonCpG,"completeData.csv")
 
 # From here onward we could check whatever!
 # If stats are computed throughout the table, then, easy to iterate N-rows at a time and compute the p-values.
@@ -146,25 +153,11 @@ results_m <- computeStatsChunks(commonCpG,chunkSize=numThr)
 results_h <- computeStatsChunks(commonCpG,mod="h",chunkSize=numThr)
 
 
-# I selected a region with a very significant corrected p-values, actually 3 regions.
-# These will be plotted
-
-#plot(commonCpG$start[commonCpG$start >=193406526 & commonCpG$end <=193419432 & commonCpG$mod=="m"], 
-#commonCpG$logMethRatio[commonCpG$start >=193406526 & commonCpG$end <=193419432 & commonCpG$mod=="m"],
-#xlab="ch4_T2T", ylab="log2(meth1/meth2)",pch=20)
-
-#abline(h=1,col="red",lty=20)
-#abline(h=-1,col="blue",lty=20)
-
-# Significant hypermethylation of H1 is observed, data saved here
-#singnR<-commonCpG[commonCpG$start >=193408987 & commonCpG$end <=193416629 & commonCpG$mod=="m" ,c("start","covM1","nmodM1","covM2","nmodM2")]
 
 # write results
 write.table(results_m,file="results_M_CpG.csv",row.names=F);
 write.table(results_h,file="results_H.CpG.csv",row.names=F);
 
-# compute results with smaller chunkSize
-#results_m_10 <- computeStatsChunks(commonCpG,chunkSize=10)
 
 ######################################################################
 # different CpG density!
@@ -215,7 +208,4 @@ countCpG<-function(meth1,meth2,chunkSize=40,mod="m",cov=5)
 
 resCpGdens<-countCpG(meth1,meth2,chunkSize=numThr);
 write.table(resCpGdens,"results_D.CpG.csv",row.names=FALSE)
-
-
-
 
