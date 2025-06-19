026b433f8548623dab1a490f5e9de8bfc81854e7
Matteo Chiara <36039934+matteo14c@users.noreply.github.com>
2025-06-19 17:49:46 +0200

    Add files via upload

--- aScanMeth.sh
+++ aScanMeth.sh
@@ -1,25 +1,40 @@
+#!/usr/bin/env bash
+
 # Path to your modkit executable
-MODKIT="../dist_modkit_v0.5.0_5120ef7/modkit"
+MODKIT="/home/spini/dist_modkit_v0.5.0_5120ef7/modkit"
 
 # Reference genome
-REF_GENOME="hs1.fa"
+REF_GENOME="/home/spini/ONTdata/hs1.fa"
+REF_FAI="/home/spini/ONTdata/hs1.fa.fai"
 
+# CPUs
+CPUS=8
 
 
 # Input BAM file
-INPUT_BAM="phased_SAMPLE_P1.bam"
-PREFIX="res_"
+INPUT_BAM="MC_phased_SAMPLE_C.bam"
+PREFIX="res_25"
+M="${PREFIX}aScan_genome_M.csv"
+C="${PREFIX}aScan_genome_C.csv"
+
 
 # chromosomes
-my_chr=("chr4" "chr10")
+
+my_chr=("chr1" "chr2" "chr3" "chr4" "chr5" "chr6" "chr7" "chr8" "chr9" "chr10" "chr11" "chr12" "chr13" "chr14" "chr15" "chr16" "chr17" "chr18" "chr19" "chr20" "chr21" "chr22")
 
 for chr in "${my_chr[@]}"; do
-	OUT="${PREFIX}${chr}"
-	echo Processing $chr
-	$MODKIT pileup --region $chr --partition-tag HP --cpg --ref $REF_GENOME --combine-strands --filter-threshold C:0.75 --mod-threshold h:0.8 $INPUT_BAM $OUT
-	cp aScanMeth.R $OUT
+        OUT="${PREFIX}${chr}"
+        echo Processing $chr
+        $MODKIT pileup --region $chr --partition-tag HP --threads $CPUS --cpg --ref $REF_GENOME --combine-strands --filter-threshold C:0.75 --mod-threshold h:0.8 $INPUT_BAM $OUT 
+        mv $OUT/1.bed $OUT/pat.bed
+        mv $OUT/2.bed $OUT/mat.bed
+	$MODKIT bm tobigwig --sizes $REF_FAI -m m $OUT/pat.bed $OUT/${chr}pat.bigWig
+	$MODKIT bm tobigwig --sizes $REF_FAI -m m  $OUT/mat.bed $OUT/${chr}mat.bigWig
+        cp aScanMeth.R $OUT
         cd $OUT
-	Rscript aScanMeth.R --h1 1.bed --h2 2.bed
-	rm aScanMethMockUp.R
-	cd ..	
+        Rscript aScanMeth.R --h1 pat.bed --h2 mat.bed --n 25 --c 10
+	#rm aScanMeth.R
+        cd ..
+	cat $OUT/results_M_CpG.csv >> $M
+	cat $OUT/results_D.CpG.csv >> $C
 done
